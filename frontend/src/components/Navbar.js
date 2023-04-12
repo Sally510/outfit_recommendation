8ce8eb9eee5978915fde1deb73620666fd610b38
Sally510 <wangqingyu510@gmail.com>
2023-04-12 21:05:07 +0800

    Checking if the user is authenticated when refresh

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../actions/auth";
 
-const Navbar = () => (
+const Navbar = ({ logout, isAuthenticated }) => {
+  const guestLinks = () => (
+    <Fragment>
+      <Link to='/login' role="button" className="btn btn-outline-light me-2">登录</Link>
+      <Link to='/register' role="button" className="btn btn-warning">注册</Link>
+    </Fragment>
+  );
+
+  const authLinks = () => (
+    <a href='#!' onClick={logout} className="btn btn-warning">注销</a>
+
+  );
+
+  return(
     <header className="p-3 text-bg-dark">
-    <div className="container">
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-        </a>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+          </a>
 
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
-          <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">About</a></li>
-        </ul>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
+            <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">About</a></li>
+          </ul>
 
-        {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
-        </form> */}
+          {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
+          </form> */}
 
-        <div className="text-end">
-            <Link to='/login' role="button" className="btn btn-outline-light me-2">登录</Link>
-            <Link to='/register' role="button" className="btn btn-warning">注册</Link>
+          <div className="text-end">
+            {isAuthenticated? authLinks(): guestLinks() }
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
 
-export default Navbar;
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
