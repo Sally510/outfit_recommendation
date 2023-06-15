d552ceb4f24ad7d9d767d5c51e22e7ee52115145
Sally510 <wangqingyu510@gmail.com>
2023-06-15 20:56:47 +0800

    registered admin

--- backend/recommendation/admin.py
+++ backend/recommendation/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
+from .models import CartItem
+from .models import HistoryItem
 from .models import Item
 # Register your models here.
 
 admin.site.register(Item)
+admin.site.register(CartItem)
+admin.site.register(HistoryItem)
